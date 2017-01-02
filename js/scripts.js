1698d22c03aa097f80f6c0e4b38d49e009af8f5d
Kevin Mizway <mizzledragon@gmail.com>
2017-01-02 18:24:29 -0500

    Program has passed fourth specification.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 function leapYear(year) {
 
-    if (year == 1993 || year % 100 == 0) {
+    if (year == 1993 || year % 100 == 0 && year % 400 !== 0) {
         return false;
     } else if (year % 4 == 0) {
         return true;
     }
-
-
-
 
 }
 
